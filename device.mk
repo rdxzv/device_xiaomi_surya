88e216cf50f4e4107d46c96a077cdfcb8c2c9efc
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-13 17:51:52 +0000

    surya: props: Enable advanced network scanning
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -417,6 +417,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.mt_sms_ack=30 \
     persist.vendor.radio.process_sups_ind=1 \
     persist.vendor.radio.report_codec=1 \
+    persist.vendor.radio.enableadvancedscan=true \
     ro.telephony.default_network=22,20
 
 # Recovery
